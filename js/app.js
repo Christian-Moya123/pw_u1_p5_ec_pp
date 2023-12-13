b9daba868bb9d365dacb8d8194214a424d1041f8
Edlith Vinueza <eavinuezaz1@uce.edu.ec>
2023-12-13 18:18:43 -0500

    taller14

--- js/app.js
+++ js/app.js
@@ -1,15 +1,35 @@
-console.log("elementos Vue")
+console.log('elemnetos Vue')
 console.log(Vue)
 
-const app =Vue.createApp({
-    // template:`
-    // <h1>Hola mundo</h1>
-    // <p>Desde App.js</p>
+const app = Vue.createApp({
+    // template: `
+    // <h1> Hola Mundo </h1>
+    // <p> Desde App.js</p>
     // <h6>{{1+1}}</h6>
-    // <p> {{[1,2,3,4,5,6,7,8]}} </p>
-    // <p> {{true?'Verdadero':'Falso'}} </p>
+    // <p>{{[1,2,3,4,5,6,7,8]}}</p>
+    // <p>{{true?'Verdadero':'Falso'}}</p>
     // `
+    methods: {
+        cambiarMensaje() {//parentecis no es necesario si no hay argumentos
+            console.log('se esta cambiando el mensaje')
+            console.log(this.mensaje)
+            mensaje = 'valor cambiado'
+        },
+        cambiarNumero() {
+            this.valor++
+            console.log(this.valor)
+        }
+    },
+    watch: {
 
+    },
+    data() {
+        return {
+            mensaje: 'hola mundo desde Vue.JS',
+            valor: 100
+        }
+    }
 })
+
 
 app.mount('#myApp')
