ac31e5e6e038ec696fb56dba31ca50c3d38412c7
Christian Moya <cmoyacharro@gmail.com>
2023-12-16 10:44:33 -0500

    taller16

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const estudiantes = [{ nombre: "alex", apellido: "moya" },
 { nombre: "christian", apellido: "charro" },
 { nombre: "alex3", apellido: "moya3" },
 { nombre: "alex4", apellido: "moy4" },
-{ nombre: "alex5", apellido: "moya5" }]
+{ nombre: "", apellido: "moya5" }]
 
 console.log(estudiantes)
 console.table(estudiantes)
@@ -39,6 +39,16 @@ const app = Vue.createApp({
             console.log("presionando")
             console.log(event.charCode)
         },
+
+        presionandoTeclaDesestrcturado({charCode}) {
+            console.log("presionando")
+            console.log(charCode)
+        },
+
+        presionandoTeclaModificador() {
+            console.log("presionando 2 ...........")
+        },
+
         presionandoEnter(event) {
             if (event.charCode === 13) {
                 const estu = { nombre: this.nombre, apellido: this.apellido2 }
